4f8b928379b72c5f67f9bf2ed73748c79ffe9c3d
Robin Jones <54465399+Rmj22@users.noreply.github.com>
2020-03-18 18:57:45 -0400

    Update index.js
    Deleted all the commented out lines

--- Develop/index.js
+++ Develop/index.js
@@ -209,7 +209,7 @@ const colors = {
           </html>`
           
           }
-//   <img class="photo-header" ${github.avatar_url}>
+
 
     inquirer.prompt([
         {
@@ -244,25 +244,6 @@ const colors = {
     })
 }
 )
-    //   .then(function({ user }) {
-    //         const queryUrl = `https://api.github.com/users/${user}`;
-        
-    //         axios.get(queryUrl).then(function(res) {
-    //           const repoNames = res.data.map(function(repo) {
-    //             return repo.name;
-    //           });
-        
-    //           const repoNamesStr = repoNames.join("\n");
-        
-    //           fs.writeFile("repos.txt", repoNamesStr, function(err) {
-    //             if (err) {
-    //               throw err;
-    //             }
-        
-    //             console.log(`Saved ${repoNames.length} repos`);
-    //           });
-    //         });
-    //   });
-
+   
 
     
